5c7bb103620a9e1ac6a6913c0329e7f682a51534
maginawin <maginawin@maginawindeMac-mini.local>
2015-01-12 15:38:29 +0800

    Initial Commit

---
+++ WwdMultiThread/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WwdMultiThread
+//
+//  Created by maginawin on 15-1-12.
+//  Copyright (c) 2015年 mycj.wwd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WwdMultiThread/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WwdMultiThread
+//
+//  Created by maginawin on 15-1-12.
+//  Copyright (c) 2015年 mycj.wwd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
